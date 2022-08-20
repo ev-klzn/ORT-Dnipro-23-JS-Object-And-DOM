79b11e686d13d49ba683b3cf8f246e41104bf3d7
ev-klzn <zhekan89@gmail.com>
2022-08-20 11:09:37 +0300

    23

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,5 @@
 function game(){
+    let secret = 57;
     let number = +numberInput.value;
     console.log(number);
 
@@ -8,5 +9,27 @@ function game(){
 
 
 
+    {
+        wrong.innerHTML = (`Ваш вариант (${number}/10)`);  
+    }
     
+        
+    if(number == secret) {
+        right.innerHTML = (`Вы угадали!!!`);
+    }
+
+        
+    if(number > secret) {
+        less.innerHTML = (`Моё число меньше ${number}`);
+    }  
+    
+    
+    if(number < secret) {
+        more.innerHTML = (`Моё число больше ${number}`);
+    }
+
+    if(number != secret) {
+        answer.innerHTML = (`Game Over!!! Ответ был ${secret}`);
+    }
+
 }
